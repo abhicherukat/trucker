c2d82d47ff2c275a5398a780a5bddecb90f49454
abhi <abhideepak22@gmail.com>
2020-08-08 16:32:24 +0530

    Updates

---
+++ precache-manifest.a2a7a819b449bfa59810370dd47b4b68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0cfabce32769184dfcfcba3cd7eec6",
+    "url": "/trucker/index.html"
+  },
+  {
+    "revision": "42d115e330205362fb4e",
+    "url": "/trucker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ddffc0b2bd7a49e5f7fc",
+    "url": "/trucker/static/css/main.fcfd7f9e.chunk.css"
+  },
+  {
+    "revision": "42d115e330205362fb4e",
+    "url": "/trucker/static/js/2.fe6d7cf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trucker/static/js/2.fe6d7cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddffc0b2bd7a49e5f7fc",
+    "url": "/trucker/static/js/main.e6b49fff.chunk.js"
+  },
+  {
+    "revision": "5789f34f65b33c1dd7df",
+    "url": "/trucker/static/js/runtime-main.7bd6db55.js"
+  }
+]);
